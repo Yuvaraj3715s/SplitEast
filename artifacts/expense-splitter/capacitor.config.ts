886e55856d7c32dece09e256c0da687ac5270b76
Ambati Yuvaraj <8203151-AmbatiYuvaraj@users.noreply.replit.com>
2026-07-04 15:09:11 +0000

    Add Android project with Capacitor

---
+++ artifacts/expense-splitter/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yuvaraj.spliteasy',
+  appName: 'SplitEasy-Yuv',
+  webDir: 'dist/public'
+};
+
+export default config;
